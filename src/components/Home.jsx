643dbee3db1258e6b4418ef2b9051ca89c81c103
Philipp Mulfinger <info@phil-splash.de>
2024-06-11 18:00:08 +0200

    change: image upload size

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -98,7 +98,7 @@ function Home() {
 
     // Verwende html2canvas, um das Bild zu erfassen
     html2canvas(document.body, {
-      scale: 0.2,
+      scale: 0.5,
     }).then((canvas) => {
       canvas.toBlob(
         async (blob) => {
@@ -117,7 +117,7 @@ function Home() {
           }
         },
         "image/jpeg",
-        0.5
+        0.7
       );
     });
   };
