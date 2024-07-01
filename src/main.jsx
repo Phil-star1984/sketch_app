7d9449769485d5c446c6ce269346ecad297d2624
Philipp Mulfinger <info@phil-splash.de>
2024-07-01 09:07:52 +0200

    fix: use vite pwa plugin for building the pwa

--- src/main.jsx
+++ src/main.jsx
@@ -4,19 +4,7 @@ import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
 
-// Check if service workers are supported
-if ("serviceWorker" in navigator) {
-  window.addEventListener("load", () => {
-    navigator.serviceWorker
-      .register("/serviceWorker.js")
-      .then((registration) => {
-        console.log("Service Worker registered: ", registration);
-      })
-      .catch((registrationError) => {
-        console.log("Service Worker registration failed: ", registrationError);
-      });
-  });
-}
+
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      includeAssets: ["favicon.svg", "robots.txt"],
+      manifest: {
+        name: "Millionpainter Sketch App",
+        short_name: "SketchApp",
+        theme_color: "#ffffff",
+        icons: [
+          {
+            src: "logo_bunt.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
+      },
+    }),
+  ],
+});
